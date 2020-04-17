8dca20a4ef795b50ca824417eb4bd1487f5170f2
USUARIO <USUARIO@DESKTOP-K6KE95E>
2020-04-17 09:33:27 -0500

    ejercicios

---
+++ laboratotio 17/laboratotio 17/laboratotio 17.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+
+/*int main()
+
+{
+
+	int x;
+	int y;
+	std::cout << "ponga un numero por favor" << std::endl;
+	std::cin >> x;
+	std::cout << "ponga un numero por favor" << std::endl;
+	std::cin >> y;
+
+	int suma = x + y;
+	int resta = x - y;
+	int producto = x * y;
+	int cociente = x % y;
+
+	std::cout << "la suma es:" << suma << std::endl;
+	std::cout << "la resta  es:" << resta <<std::endl;
+	std::cout << "la multiplicacion  es:" << producto << std::endl;
+	std::cout << "el cociente es:" << cociente << std::endl;
+
+	return 0;
+
+
+}*/
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+/*int main()
+
+{
+	int x;
+	int y;
+	std::cout << "ponga un numero por favor" << std::endl;
+	std::cin >> x;
+	std::cout << "ponga un numero por favor" << std::endl;
+	std::cin >> y;
+
+	if (x > y) {
+
+		std::cout << "el numero mayor es " << x << std::endl;
+	}
+
+	else if (x < y) {
+		std::cout << "el numero mayor es " << y << std::endl;
+	}
+
+	else {
+		std::cout << "estos numeros son iguales " << std::endl;
+	}
+
+	return 0;
+
+}*/
+
+/*int main()
+
+{
+
+	int x;
+	int y;
+	int z;
+
+
+
+	std::cout << "ponga un numero por favor" << std::endl;
+	std::cin >> x;
+	std::cout << "ponga un numero por favor" << std::endl;
+	std::cin >> y;
+	std::cout << "ponga un numero por favor " << std::endl;
+	std::cin >> z;
+
+	int suma = x + y + z;
+	int producto = x * y * z;
+	int promedio = (x + y + z) / 3;
+
+	std::cout << "la suma es: " << suma << std::endl;
+	std::cout << "el promedio es: " << promedio << std::endl;
+	std::cout << "el producto es:  " << producto << std::endl;
+
+	if (x > y && x > z) {
+
+		std::cout << "el numero mayor es " << x << std::endl;
+	}
+
+	else if (y > x && y > z) {
+		std::cout << "el numero mayor es " << y << std::endl;
+	}
+	else if (z > x && z > y) {
+		std::cout << "el numero mayor es " << z << std::endl;
+	}
+	
+	else {
+		std::cout << "estos numeros son iguales " << std::endl;
+	}
+
+	return 0;
+
+
+
+}*/
+
+
+
+
+
+
+
+
